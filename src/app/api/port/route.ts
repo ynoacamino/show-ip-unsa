33ec2ddf165b06a3ba642a4e6162c41a213f4b35
Yenaro Joel Noa Camino <97652778+ynoacamino@users.noreply.github.com>
2024-07-10 03:47:49 +0000

    update revalidate rule

--- src/app/api/port/route.ts
+++ src/app/api/port/route.ts
@@ -2,8 +2,9 @@ import {prisma} from "@/lib/prisma"
 import { revalidatePath } from "next/cache";
 
 export async function POST(req: Request) { 
+    revalidatePath("/", "page")
     const body = await req.json()
-
+    
     if (!body?.ip) {
         return Response.json({ message: "Envia la ip en un un obejto en el body"}, {status: 400})
     }
@@ -20,7 +21,6 @@ export async function POST(req: Request) {
         return Response.json({ message: "Error al subir a la base de datos"}, {status: 500})
     }
 
-    revalidatePath("/", "page")
 
     return Response.json(port)
 }
